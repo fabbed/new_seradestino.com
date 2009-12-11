13388ca358f0074c7424eba071c6319ae38986eb
Fabian Dittrich <fabian.dittrich@gmail.com>
2009-12-11 19:05:46 +0100

    gfdgfdsgfds

--- app/models/user_observer.rb
+++ app/models/user_observer.rb
@@ -2,13 +2,13 @@ class UserObserver < ActiveRecord::Observer
   def after_create(user)
     user.reload
     puts "Observer after create"
-    UserMailer.deliver_signup_notification(user)
+    UserMailer.registered(user)
   end
 
   def after_save(user)
     user.reload
     puts "Observer after save"          
-    UserMailer.deliver_activation(user) if user.recently_activated?
+    UserMailer.activated(user) if user.recently_activated?
   
   end
 end
