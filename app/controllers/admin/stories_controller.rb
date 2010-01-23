a1255f452a210c183a2cae6e7a0777704cce02cf
Fabian Dittrich <fdi@betterplace.org>
2010-01-23 17:44:09 +0100

    hgf

--- app/controllers/admin/stories_controller.rb
+++ app/controllers/admin/stories_controller.rb
@@ -22,24 +22,24 @@ class Admin::StoriesController < Admin::MasterController
   #     Story: create, read, update, custom_action_for_an_item
   #
 
-  def index
-
-
-
-    @conditions, @joins = @resource[:class].build_conditions(params)
-    @conditions = "(`stories`.`experiment_story` = 1)"
-    check_ownership_of_items if @resource[:class].typus_options_for(:only_user_items)
-
-    respond_to do |format|
-      format.html { generate_html }
-      @resource[:class].typus_export_formats.each do |f|
-        format.send(f) { send("generate_#{f}") }
-      end
-    end
-
-  rescue Exception => error
-    error_handler(error)
-  end
+  # def index
+  # 
+  # 
+  # 
+  #   @conditions, @joins = @resource[:class].build_conditions(params)
+  #   @conditions = "(`stories`.`experiment_story` = 1)"
+  #   check_ownership_of_items if @resource[:class].typus_options_for(:only_user_items)
+  # 
+  #   respond_to do |format|
+  #     format.html { generate_html }
+  #     @resource[:class].typus_export_formats.each do |f|
+  #       format.send(f) { send("generate_#{f}") }
+  #     end
+  #   end
+  # 
+  # rescue Exception => error
+  #   error_handler(error)
+  # end
   
   # def custom_action
   # end
--- lib/tasks/get_countries.rake
+++ lib/tasks/get_countries.rake
@@ -16,7 +16,8 @@ namespace :stories do
       
 
       [1, 2, 3, 4, 5, 8, 10, 11, 21, 24, 46, 73, 81, 86, 98, 106, 110, 113, 118, 119, 121, 122, 137, 141, 142, 146, 147, 155, 157, 173, 177, 182, 186, 191].each do |id|
-        Story.find(id).update_attribute(:fab_rating, 2)
+        Story.find(id).update_attribute(:fab_rating, 0)
+        Story.find(id).update_attribute(:experiment_story, true)        
 
     end
   end
