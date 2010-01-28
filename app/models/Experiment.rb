fcedb22b44969dfede14b11ef104a6fb9b88975b
Fabian Dittrich <fdi@betterplace.org>
2010-01-28 13:03:38 +0100

    gdfs

--- app/models/Experiment.rb
+++ app/models/Experiment.rb
@@ -3,20 +3,15 @@ class Experiment
   @@MAN_LEV = ["symbols", "dialect", "ugc"]
   @@APT_LEV = ["ne", "mx", "es"]
 
-  @@MX_USERS = [66,49,43, 18, 10, 11]
-  @@ES_USERS = [1]
+  @@NEUTRAL_USERS = [280, 224, 46, 197, 128, 137, 147]
 
 
   def self.get_variation
       @@MAN_LEV[rand(3)]+"_"+@@APT_LEV[rand(3)]
   end
 
-  def self.get_mexican_users
-    User.find(@@MX_USERS, :order => "rand()")
-  end
-
-  def self.get_spanish_users
-    User.find(@@ES_USERS, :order => "rand()")
+  def self.get_neutral_users
+    User.find(@@NEUTRAL_USERS, :order => "rand()")
   end
 
   def self.get_experiment_stories
